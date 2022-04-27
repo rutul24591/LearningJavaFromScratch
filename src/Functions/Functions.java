ffb5904c53219475ab7dd8d9c87515b98b0f02f0
Rutul Amin <rutulamin.ra@gmail.com>
2022-04-27 15:21:18 +0530

    Modifying the functions class

--- src/Functions/Functions.java
+++ src/Functions/Functions.java
@@ -20,6 +20,58 @@ package Functions;
 *       This method doesn't get any arguments, but Java methods can get arguments.
 *
 *
+*   ARGUMENTS
+*
+        I always like to say that arguments to Java methods are passed by value, although some might disagree with my choice of words,
+        I find it the best way to explain and understand how it works exactly.
+
+        By value means that arguments are copied when the method runs. Let's look at an example.
+
+        public void bar(int num1, int num2) {
+            ...
+        }
+
+        Here is a another place in the code, where bar is called
+
+        int a = 3;
+        int b = 5;
+        bar(a, b);
+
+        You can picture in your head that when bar(a, b) is run, it's like in the beginning of bar the following two lines are written:
+
+        int num1 = a;
+        int num2 = b;
+
+        And only then the rest of the method is run.
+
+        This means that a value get copied to num1 and b value get copied to num2. Changing the values of num1 and num2 will not affect a and b.
+
+        If the arguments were objects, the rules remain the same, but it acts a bit differently. Here is an example:
+
+        public void bar2(Student s1, Student s2) {
+            ...
+        }
+
+        And here is how we use it
+
+        Student joe = new Student("joe");
+        Student jack = new Student("jack");
+        bar2(joe, jack);
+
+        Again we can picture the same two lines in the beginning of bar2
+
+        Student s1 = joe;
+        Student s2 = jack;
+
+        But when we assign objects, it's a bit different than assigning primitives. s1 and joe are two different references to the same object.
+        s1 == joe is true. This means that running methods on s1 will change the object joe.
+        But if we'll change the value of s1 as a reference, it will not affect the reference joe.
+
+        s1.setName("Chuck"); // joe name is now Chuck as well
+        s1 = new Student("Norris"); // s1 is a new student, different than joe with the name of Norris
+        // s1 == joe   is not true anymore
+*
+*
 *   NON-STATIC METHODS
 *
 *       Non-static methods in Java are more used than static ones.
