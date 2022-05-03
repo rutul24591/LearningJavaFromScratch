fec7fb86b6125a91123ce60d7ab385d970387189
Rutul Amin <rutulamin.ra@gmail.com>
2022-05-03 15:03:52 +0530

    Added addNodeAtEnd to DoublyLinkedList

--- src/DoublyLinkedList/LinkedList.java
+++ src/DoublyLinkedList/LinkedList.java
@@ -1,4 +1,35 @@
 package DoublyLinkedList;
 
 public class LinkedList {
+    Node head;
+    public void addNodeAtEnd(int data){
+        Node node = new Node();
+        node.data = data;
+        node.prev = null;
+        node.next = null;
+
+        // Check if the element to be inserted is the first element in the list
+        if(head == null){
+            head = node;
+        } else {
+            Node n = head;
+
+            //Traverse the list until last element is reached
+            while(n.next != null){
+                n = n.next;
+            }
+            n.next = node;
+            node.prev = n;
+        }
+    }
+
+    public void display(){
+        Node n = head;
+        while(n.next != null){
+            System.out.println("Node Data:" +n.data);
+            n = n.next;
+        }
+        // Without this line the last elements' data won't be printed, as loop runs until the second last element
+        System.out.println("Node Data:"+n.data);
+    }
 }
--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -1,4 +1,16 @@
 package DoublyLinkedList;
 
 public class Main {
+    public static void main(String[] args){
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.addNodeAtEnd(1);
+        linkedList.addNodeAtEnd(2);
+        linkedList.addNodeAtEnd(3);
+        linkedList.addNodeAtEnd(4);
+        linkedList.addNodeAtEnd(5);
+
+        linkedList.display();
+
+    }
 }
