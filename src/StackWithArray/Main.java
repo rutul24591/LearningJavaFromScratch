6135d144cfd3357caed3a369ca6454dde8e046b9
Rutul Amin <rutulamin.ra@gmail.com>
2022-05-03 17:43:26 +0530

    Added basic stack implementation

---
+++ src/StackWithArray/Main.java
@@ -0,0 +1,20 @@
+package StackWithArray;
+
+public class Main {
+    public static void main(String[] args){
+        IntegerStack stack = new IntegerStack(50);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println("Stack Size is: " + stack.size());
+
+        System.out.println("Is stack empty? " + stack.isEmpty());
+        System.out.println("Is stack full? " + stack.isFull());
+        System.out.println("What is the top of stack? " + stack.top());
+
+    }
+}
