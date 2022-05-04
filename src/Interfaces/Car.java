148f7d2d61729e6b20674008c13472c959b15755
Rutul Amin <rutulamin.ra@gmail.com>
2022-05-04 15:44:45 +0530

    Added Few TODO Concepts yet to be done and completed Inheritance part.

---
+++ src/Interfaces/Car.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+class Car extends Vehicle{
+    // Settings modelName as protected instead of private to make it accessible by Main class
+    protected String modelName = "Mustang";
+}
---
+++ src/Interfaces/Vehicle.java
@@ -0,0 +1,21 @@
+package Interfaces;
+/*
+*  In Java, inheritance is the way by which a class(subclass) can access properties and attributes from another class(superclass)
+
+        subclass (child) - the class that inherits from another class
+        superclass (parent) - the class being inherited from
+
+* To inherit from a class, use the extends keyword.
+
+In the example below, the Car class (subclass) inherits the attributes and methods from the Vehicle class (superclass):
+*/
+public class Vehicle {
+    // We set the brand attribute in Vehicle to a protected access modifier.
+    // If it was set to private, the Car class would not be able to access it.
+    protected String brand = "Ford";
+    public void honk() {                    // Vehicle method
+        System.out.println("Beep, beep!");
+    }
+}
+
+
