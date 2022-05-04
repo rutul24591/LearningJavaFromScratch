6e99759f9eee022bf8f91fc51e90712b82542ce1
Rutul Amin <rutulamin.ra@gmail.com>
2022-05-04 16:00:22 +0530

    Added how to comment in java

---
+++ src/Comments/Main.java
@@ -0,0 +1,27 @@
+package Comments;
+
+/*
+    JAVA COMMENTS
+        It can be used to explain Java code, and to make it more readable. It can also be used to prevent execution when testing alternative code.
+
+        Single-line Comments
+            Single-line comments start with two forward slashes (//).
+
+            Any text between // and the end of the line is ignored by Java (will not be executed).
+
+        Multi-line Comments
+            Multi-line comments start and ends with /* */
+
+
+public class Main {
+    public static void main(String[] args){
+        // This is single line comments in java
+        System.out.println("Single line comment demo"); // This is a comment
+
+        /*
+             The code below will print the words Multi line comment demo
+            to the screen. This is a multi line comments in java
+        */
+        System.out.println("Multi line comment demo");
+    }
+}
